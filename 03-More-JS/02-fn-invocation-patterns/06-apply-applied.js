a7ec459534ee0d5e1aaaa28f39111bfe34b497a3
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-21 01:29:21 +0530

    updates

--- 03-More-JS/02-fn-invocation-patterns/06-apply-applied.js
+++ 03-More-JS/02-fn-invocation-patterns/06-apply-applied.js
@@ -27,3 +27,36 @@ logAdd(10, 20, 30, "40", 50) //=> 150
 logAdd(10, 20, 30, 40, 50, "abc") //=> 150
 
 //modify the profile() function accordingly
+//profiling
+function profile(operationFn) {
+    return function () {
+        console.time('operation')
+        operationFn.apply(this, arguments)
+        console.timeEnd('operation')
+    }
+}
+
+var profileAdd = profile(add)
+profileAdd(10, 20) //=> 30
+profileAdd(10) //=> 10
+profileAdd() //=> 0 
+profileAdd(10, 20, 30, 40, 50) //=> 150
+profileAdd("10") //=> 10
+profileAdd("abc") //=> 0
+profileAdd(10, 20, 30, "40", 50) //=> 150
+profileAdd(10, 20, 30, 40, 50, "abc") //=> 150
+
+
+
+// combining logging & profiling (function composition)
+var logAdd = logOperation(add)
+var profileLogAdd = profile(logAdd)
+profileLogAdd(10, 20) //=> 30
+profileLogAdd(10) //=> 10
+profileLogAdd() //=> 0 
+profileLogAdd(10, 20, 30, 40, 50) //=> 150
+profileLogAdd("10") //=> 10
+profileLogAdd("abc") //=> 0
+profileLogAdd(10, 20, 30, "40", 50) //=> 150
+profileLogAdd(10, 20, 30, 40, 50, "abc") //=> 150
+
