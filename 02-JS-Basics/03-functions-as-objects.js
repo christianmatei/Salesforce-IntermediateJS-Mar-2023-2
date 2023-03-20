0e10d6593814660fec2b1a74cafc3d7c03f75598
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-20 21:18:06 +0530

    methods on functions

--- 02-JS-Basics/03-functions-as-objects.js
+++ 02-JS-Basics/03-functions-as-objects.js
@@ -40,3 +40,23 @@ function fn() {
 fn['id'] = 100
 console.log(fn.id)
 //output : 100
+
+
+//3: Functions can have Methods (like objects)
+//Object
+var product = {}
+product['whoAmI'] = function () {
+    console.log('I am a Product');
+}
+product.whoAmI()
+//output: I am a Product
+
+//Function
+function fn() {
+    console.log('fn invoked')
+}
+fn['whoAmI'] = function () {
+    console.log('I am a function');
+}
+fn.whoAmI()
+//output: I am a function
