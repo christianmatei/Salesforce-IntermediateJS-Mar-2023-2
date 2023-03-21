a7992d3cd80008fb9d3454a5a402366032521c9d
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-21 21:03:39 +0530

    Generic memoize - v1.0

--- 03-More-JS/03-closures/03-memoization.js
+++ 03-More-JS/03-closures/03-memoization.js
@@ -14,3 +14,63 @@ var isPrime = (function(){
         return cache[no]
     }
 })()
+
+// Create a memoized version of 'isOdd' & 'isEven' functions
+/* var isEven = (function () {
+    var cache = {}
+    function isEven(no) {
+        console.log('processing ', no)
+        return no % 2 === 0 ? true : false
+    }
+    return function (no) {
+        if (cache.hasOwnProperty(no))
+            return cache[no]
+        cache[no] = isEven(no)
+        return cache[no]
+    }
+})()
+
+var isOdd = (function () {
+    var cache = {}
+    function isOdd(no) {
+        console.log('processing ', no)
+        return no % 2 !== 0 ? true : false
+    }
+    return function (no) {
+        if (cache.hasOwnProperty(no))
+            return cache[no]
+        cache[no] = isOdd(no)
+        return cache[no]
+    }
+})()
+
+ */
+
+// Generic memoize - v1.0
+function memoize(fn) {
+    var cache = {}
+    return function (no) {
+        if (cache.hasOwnProperty(no))
+            return cache[no]
+        cache[no] = fn(no)
+        return cache[no]
+    }
+}
+
+var isPrime = memoize(function(no) {
+    console.log('processing ', no)
+    for (var i = 2; i <= (no / 2); i++) {
+        if (no % i === 0) return false
+    }
+    return true
+})
+
+var isEven = memoize(function (no) {
+    console.log('processing ', no)
+    return no % 2 === 0 ? true : false
+})
+
+var isOdd = memoize(function (no) {
+    console.log('processing ', no)
+    return no % 2 !== 0 ? true : false
+})
