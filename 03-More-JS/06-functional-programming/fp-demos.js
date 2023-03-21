ce47b263e01cb4ef2cdd0ebc412263578cfba4cd
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-21 23:57:34 +0530

    fp - filter - assignment

---
+++ 03-More-JS/06-functional-programming/fp-demos.js
@@ -0,0 +1,15 @@
+var products = [
+    { id: 6, name: 'Pen', cost: 50, units: 20, category: 'stationary' },
+    { id: 9, name: 'Ten', cost: 70, units: 70, category: 'stationary' },
+    { id: 3, name: 'Len', cost: 60, units: 60, category: 'grocery' },
+    { id: 5, name: 'Zen', cost: 30, units: 30, category: 'grocery' },
+    { id: 1, name: 'Ken', cost: 20, units: 80, category: 'utencil' },
+    { id: 7, name: 'Mouse', cost: 100, units: 20, category: 'electronics' }
+];
+
+console.log('Initial List')
+console.table(products)
+
+// Filter (use the array.filter() method)
+// Filter all costly products (cost > 50) (result => id(9,3,7) )
+// Filter all affordable products (result => id(6,5,1))
