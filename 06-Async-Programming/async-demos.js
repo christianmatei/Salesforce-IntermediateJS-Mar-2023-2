e201ba7d22887a0319e2321779f7f17460c24e70
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-22 21:15:41 +0530

    async - using callbacks

--- 06-Async-Programming/async-demos.js
+++ 06-Async-Programming/async-demos.js
@@ -16,19 +16,21 @@
     window['addSyncClient'] = addSyncClient;
 
     // Async
-    function addAsync(x, y) {
-        console.log(`   [@service] processing ${x} and ${y}`)
+    function addAsync(x, y, callback) {
+        console.log(`   [@service] initializing processing ${x} and ${y}`)
         setTimeout(function(){
             const result = x + y
             console.log(`   [@service] returning the result`)
-            return result
-        }, 5000);
+            if (typeof callback === 'function')
+                callback(result)
+        }, Math.random() * 10000);
     }
 
     function addAsyncClient(x, y) {
         console.log(`[@client] invoking the service`)
-        const result = addAsync(x, y)
-        console.log(`[@client] operation completed. result = ${result}`)
+        addAsync(x, y, function(result){
+            console.log(`[@client] operation completed. result = ${result}`)
+        })
     }
     window['addAsyncClient'] = addAsyncClient;
 
